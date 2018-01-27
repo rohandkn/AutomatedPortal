904a2d4a1ea1fa358ec0a6760bdfdcf97aae2e79
Rohan Dhesikan <rohandhesikan@gmail.com>
2018-01-27 13:41:31 -0800

    Updated

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+try:
+    from selenium import webdriver
+    from selenium.webdriver import ActionChains
+    from selenium.webdriver.common.keys import Keys
+except:
+    "You don't have Selenium installed. This is required to run the program. In your terminal, run 'pip install selenium'"
+import subprocess
+import csv
+
+emails = []
+urls = []
+passwords = []
+
+output = subprocess.run("pwd", shell=True, stdout=subprocess.PIPE,
+                        universal_newlines=True)
+location = str(output.stdout)+'/chromedriver'
+location = location.replace("\n","")
+print(location)
+chromedriver = location
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+counter = 1
+with open('links.csv', 'rt') as csvfile:
+    spamreader = csv.DictReader(csvfile)
+    for column in spamreader:
+         emails.append(column['user'])
+         passwords.append(column['pass'])
+         urls.append(column['url'])
+
+
+for item in urls:
+    try:
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[counter])
+        driver.get(item)
+        try:
+            username1 = driver.find_elements_by_css_selector("input")
+            username = username1[0]
+            password = username1[1]
+
+            username.send_keys(emails[counter-1])
+            password.send_keys(passwords[counter-1])
+
+            driver.find_element_by_css_selector("button").click()
+            try:
+                driver.switch_to.alert().accept()
+            except:
+                print("No Alert")
+            print("Fully logged in for " + item)
+        except:
+            print("Unable to fully login for "+item+". Still, the website will still be loaded")
+    except:
+        print("Something weird went wrong")
+    counter += 1
+
+
+
+
+
+
+
+
+
+
